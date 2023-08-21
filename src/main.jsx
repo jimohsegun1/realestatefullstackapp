87f489da967ab5ecf71b2356b9857b113f2298b5
jimohsegun1 <99956721+jimohsegun1@users.noreply.github.com>
2023-08-21 17:11:24 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-s2g4zuot5bajveg2.us.auth0.com"
+     clientId="6KYT3D39Dsj1Q5QBkRO7UsdSM6igF5IM"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
