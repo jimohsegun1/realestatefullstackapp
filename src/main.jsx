6e8d7b9bd8c89acb143c66ce15edd9f50b735280
jimohsegun1 <99956721+jimohsegun1@users.noreply.github.com>
2023-08-21 17:25:02 +0100

    Update main.jsx with auth0 details

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-s2g4zuot5bajveg2.us.auth0.com"
      clientId="6KYT3D39Dsj1Q5QBkRO7UsdSM6igF5IM"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://realestatefullstackapp.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
